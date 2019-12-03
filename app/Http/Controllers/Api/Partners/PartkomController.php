739f67b99d280a433aab801339b6280f8293fc6c
Merecoin <36365036+merecoin@users.noreply.github.com>
2019-12-03 19:17:34 +0300

    Partkom get data working properly

--- app/Http/Controllers/Api/Partners/PartkomController.php
+++ app/Http/Controllers/Api/Partners/PartkomController.php
@@ -9,6 +9,41 @@ class PartkomController extends CommonParentController
 {
     public function main($query)
     {
-        return 'PartkomController';
+        $url = $this->prepareUri();
+        $headers = $this->prepareHeaders();
+        $queryToGuzzle = $this->prepareQuery($query);
+        $responseFromExtApi = $this->doGuzzle($url, $headers, $queryToGuzzle);
+        return $responseFromExtApi;
     }
+
+    public function prepareUri()
+    {
+        return $this->partnerModelData->url;
+    }
+
+    public function prepareHeaders()
+    {
+        $auth64 = 'Basic ' . base64_encode($this->partnerModelData->login . ':' . $this->partnerModelData->password);
+        return [
+            'Authorization' => $auth64,
+            'Accept' => 'application/json',
+            'Content-type' => 'application/json',
+        ];
+    }
+
+    public function prepareQuery(string $query)
+    {
+        return ['number' => $this->getFirstArticleFromQuery($query)];
+    }
+
+    public function doGuzzle(string $url, array $headers, array $query)
+    {
+        $client = new \GuzzleHttp\Client(['base_uri' => $url, 'verify' => false]);
+        $apiResponse = $client->request('GET', '', [
+            'query' => $query,
+            'headers' => $headers,
+        ]);
+        return $apiResponse->getBody()->getContents();
+    }
+
 }
